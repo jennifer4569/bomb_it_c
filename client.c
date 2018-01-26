558789829e0c351b21228227b8ff87fc513d528d
clei1 <clei1@stuy.edu>
2018-01-26 00:18:11 -0500

    better thing?

--- client.c
+++ client.c
@@ -15,55 +15,44 @@ void windowSetup(){
 
 void getKeys(int* ch1, int* ch2){
   int ch;
-  clock_t begin;
-  double time_spent;
-  begin = clock();
-  while(1){
-    if ((ch = getch()) == ERR) {
-      //user hasn't moved
-    }
-    else {
-      if (*ch1 == ' ' && *ch2 == ' '){
-	*ch2 = ch;
+  *ch1 = getch();
+  if (*ch1 == ' '){
+    while(1){
+      *ch2 = getch();
+      if(*ch2 == ERR){
+	*ch2='\0';
+	break;
       }
-      if( *ch1 == 'x' ){
-	*ch1 = ch;
+      if(*ch2 != ' '){
+	break;
       }
-    }
-    time_spent = (double)(clock() - begin) / CLOCKS_PER_SEC;
-    if(time_spent >= 0.5){
-      break;
     }
   }
 }
 
 void resetKeys(int * ch1, int * ch2){
-  *ch1 = 'x';
-  *ch2 = ' ';
+  *ch1 = '\0';
+  *ch2 = '\0';
 }
 
 int main(){
-  //client establishes connection with server
-  //INSERT CLIENT SERVER HANDSHAKE STUFF
 
   windowSetup();
   
-  int ch1 = 'x';//default values
-  int ch2 = ' ';//default values
-  char buffer[3] = { ch1, ch2, '\0'};
+  int ch1 = 'w';//default values
+  int ch2 = '\0';//default values
 
-  //recieves first map
-  char map[MAP_SIZE];
-  //INSERT STARTING MAP, READ FROM SERVER
-  //read(server_socket, map, sizeof(map));
-
+  sleep(4);
+  //getKeys(&ch1, &ch2);
+  if(ch1){
+    printw("Hello world!");
+  }
+  //printw("|%c%c|", ch1, ch2);
+  refresh();
+  sleep(10);
+  /*
+  
   while(1){//game loop
-    
-    //reads map from server
-    select(server_socket + 1, &read_fds, NULL, NULL, NULL);
-    read(server_socket, map, sizeof(map));
-    clear(); //clears screen
-    printw(map); //prints updated map
 
     getKeys(&ch1, &ch2); //keyboard interception, runs for .5 seconds
 
@@ -76,7 +65,7 @@ int main(){
       resetKeys(&ch1, &ch2); //resets keys after writing
     }
   }
-  
+  */
   endwin(); //restore normal terminal behavior
   return 0;
 }
