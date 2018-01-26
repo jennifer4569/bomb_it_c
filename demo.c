36285d1fd7dd6832ea0aaa45a062f56850c5756b
clei1 <clei1@stuy.edu>
2018-01-25 21:53:23 -0500

    client side stuff

--- demo.c
+++ demo.c
@@ -13,29 +13,22 @@ void windowSetup(){
   //getch() returns ERR if key input is not read
 }
 
-int main(){
-  char ch;
-  //int x = 0;
-  //int y = 0;
-
-  
-  windowSetup();
-  /*
-  char ch1 = 'x';
-  char ch2 = ' ';
+void getKeys(int* ch1, int* ch2){
+  int ch;
   clock_t begin;
   double time_spent;
+  sleep(1);
   begin = clock();
-  while (1){
+  while(1){
     if ((ch = getch()) == ERR) {
       //user hasn't moved
     }
     else {
-      if (ch1 == ' ' && ch2 == ' '){
-	ch2 = ch;
+      if (*ch1 == ' ' && *ch2 == ' '){
+	*ch2 = ch;
       }
-      if( ch1 == 'x' ){
-	ch1 = ch;
+      if( *ch1 == 'x' ){
+	*ch1 = ch;
       }
     }
     time_spent = (double)(clock() - begin) / CLOCKS_PER_SEC;
@@ -43,31 +36,45 @@ int main(){
       break;
     }
   }
-  */
+}
 
-  char ch1 = 'x';
-  char ch2 = ' ';
-  sleep(5);
-  int i = 100;
-  for(int i = 0; i > 0; i--){
-    if ((ch = getch()) == ERR) {
-      //user hasn't moved
+int main(){
+  //client establishes connection with server
+  windowSetup();
+  int ch1 = 'x';//default values
+  int ch2 = ' ';//default values
+  int st = 1; //1 means server has sent updated map, 0 means same map
+
+  //recieves first map
+  //char * map =
+
+  while(1){//game loop
+    if (st) { //1 means server has sent updated map
+      clear(); //clears screen
+      printw(map); //prints updated map
+      st = 0; //sets status back to 0
     }
-    else {
-      if (ch1 == ' ' && ch2 == ' '){
-	ch2 = ch;
+    
+    //reads from server the map
+    //read(server_socket, map, sizeof(map));
+    //st = 1;
+
+    getKeys(&ch1, &ch2); //keyboard interception
+
+    if( ch1 != 'x'){
+      char buffer[3] = { ch1, ch2, '\0'};
+      if( ch1 != ' '){
+        buffer[2] = '\0';
       }
-      if(ch1 == 'x'){
-	ch1 = ch;
-      }
+      //write(server_socket, buffer, sizeof(buffer));
+      resetKeys(&ch1, &ch2); //resets keys after reading
     }
   }
   
-  printw(&ch1);
-  printw(&ch2);
-  refresh();  
+  //printw("%c%c", ch1, ch2);
+  //refresh();  
 
-  sleep(4);
+  //sleep(4);
   endwin(); //restore normal terminal behavior
   return 0;
 }
